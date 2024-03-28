624a511c1f3f845dc99ce6d9051b24df513c1fad
Maksim Skobun <max@skobun.me>
2024-03-28 18:29:39 +0800

    Database: Add migration and seeder for Menu items/categories

---
+++ database/migrations/2024_03_28_093643_create_menu_items_and_categories_tables.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("description");
+            $table->float("price");
+            $table->boolean("available");
+        });
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+        });
+        Schema::create('items_categories', function (Blueprint $table) {
+            $table->foreignId("item_id");
+            $table->foreignId("category_id");
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+        Schema::dropIfExists('categories');
+        Schema::dropIfExists('items_categories');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -19,5 +20,35 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+        DB::table('categories')->insert([
+            ['id' => 1, 'name' => 'Burgers'],
+            ['id' => 2, 'name' => 'Desserts & Sides'],
+            ['id' => 3, 'name' => 'Drinks']]);
+        DB::table('items')->insert([
+            ['id' => 1, 'name' => 'Big Mac', 'description' => 'Two all-beef patties with lettuce, onions, pickles, cheese and special sauce in a toasted sesame seed bun.', 'price' => 14.20, 'available' => true, 'image_url' => '/storage/mcd.png'],
+            ['id' => 2, 'name' => 'Spicy Chicken McDeluxe', 'description' => 'Spicy Chicken McDeluxe™', 'available' => true, 'price' => 14.35, 'image_url' => '/storage/mcd.png'],
+            ['id' => 3, 'name' => 'Filet-O-Fish', 'description' => 'A classic favourite of a fish burger served with tartar sauce and cheddar cheese in a steamed bun.', 'available' => true, 'price' => 10.55, 'image_url' => '/storage/mcd.png'],
+            ['id' => 4, 'name' => 'Cheeseburger', 'description' => 'Complement the taste and pure Beef Burger with a slice of cheese.', 'available' => true, 'price' => 5.99, 'image_url' => '/storage/mcd.png'],
+            ['id' => 5, 'name' => 'French fries', 'description' => '', 'available' => true, 'price' => 6.70, 'image_url' => '/storage/mcd.png'],
+            ['id' => 6, 'name' => 'Oreo McFlurry', 'description' => '', 'available' => true, 'price' => 8.25, 'image_url' => '/storage/mcd.png'],
+            ['id' => 7, 'name' => 'Apple Pie', 'description' => '', 'available' => true, 'price' => 5.25, 'image_url' => '/storage/mcd.png'],
+            ['id' => 8, 'name' => 'Coca Cola', 'description' => '', 'available' => true, 'price' => 5.80, 'image_url' => '/storage/mcd.png'],
+            ['id' => 9, 'name' => 'Sprite', 'description' => '', 'available' => true, 'price' => 5.80, 'image_url' => '/storage/mcd.png'],
+            ['id' => 10, 'name' => 'Iced Milo', 'description' => '', 'available' => true, 'price' => 8.30, 'image_url' => '/storage/mcd.png'],
+
+        ]);
+        DB::table('items_categories')->insert([
+            ['item_id' => 1, 'category_id' => 1],
+            ['item_id' => 2, 'category_id' => 1],
+            ['item_id' => 3, 'category_id' => 1],
+            ['item_id' => 4, 'category_id' => 1],
+            ['item_id' => 5, 'category_id' => 2],
+            ['item_id' => 6, 'category_id' => 2],
+            ['item_id' => 7, 'category_id' => 2],
+            ['item_id' => 8, 'category_id' => 3],
+            ['item_id' => 9, 'category_id' => 3],
+            ['item_id' => 10, 'category_id' => 3]
+        ]);
+
     }
 }
