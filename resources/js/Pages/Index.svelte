8c4424053d593c3d71dafd39043a1bef09dd3b76
Maksim Skobun <max@skobun.me>
2024-04-02 12:42:36 +0800

    Split MenuItem into a separate component
    Will be useful for cart/checkout views later

--- resources/js/Pages/Index.svelte
+++ resources/js/Pages/Index.svelte
@@ -1,7 +1,7 @@
 <script>
     import { inertia, Link, router } from "@inertiajs/svelte";
     import Layout from "../Shared/Layout.svelte";
-    import { Button, ButtonGroup } from "flowbite-svelte";
+    import MenuItem from "../Shared/MenuItem.svelte";
 
     export let menu;
     export let cart = {};
@@ -27,74 +27,15 @@
                 <h1 class="font-bold text-3xl py-3">{category}</h1>
                 <div class="md:grid md:grid-cols-3 md:gap-3 max-md:divide-y">
                     {#each items as item}
-                        <!-- The div is necessary here because we use 2 borders
-                    with different colors and widths, one for dividing the items,
-                    other is a side stripe to identify items in the cart.
-                    So we put the former on this div, and the latter on the div
-                    inside it. -->
-                        <div>
-                            <!-- Ideally, the classes should not be repeated,
-                        we should be able to do something like:
-                        baseClasses + (ordered) ? red border : white border
-                        However, for some reason, if you do that,
-                        the border-primary-600 style will not be present in
-                        the compiled css. I guess tailwind only generates them
-                        if it sees a usage, and the logic becomes too complicated
-                        for it to figure it out. -->
-                            <div
-                                class={cart[item.id] !== undefined
-                                    ? "border-primary-600 border-l-4 p-1 flex md:rounded-md gap-3 bg-white"
-                                    : "border-white border-l-4 p-1 flex md:rounded-md gap-3 bg-white"}
-                            >
-                                <img
-                                    class="h-20 w-20"
-                                    src={item.image_url}
-                                    alt={item.name}
-                                />
-                                <h2 class="text-lg text-black">
-                                    {item.name}
-                                </h2>
-                                <div
-                                    class="mr-1 ml-auto flex-col flex items-end"
-                                >
-                                    <h2 class="font-bold text-l">
-                                        {item.price.toFixed(2)}
-                                    </h2>
-                                    <div class="mb-2 mt-auto">
-                                        {#if cart[item.id] === undefined}
-                                            <Button
-                                                on:click={() =>
-                                                    modifyCart(item.id, 1)}
-                                                class="rounded-full w-8 h-8 p-2 inline-flex text-center text-xl"
-                                            >
-                                                +
-                                            </Button>
-                                        {:else}
-                                            <ButtonGroup size="xs" class="h-8">
-                                                <Button
-                                                    pill
-                                                    on:click={() => {
-                                                        modifyCart(item.id, -1);
-                                                    }}>-</Button
-                                                >
-                                                <!-- Not really a button, but I just wanted to use the ButtonGroup class -->
-                                                <Button pill>
-                                                    {cart[item.id]}
-                                                </Button>
-                                                <Button
-                                                    pill
-                                                    on:click={() => {
-                                                        modifyCart(item.id, 1);
-                                                    }}
-                                                >
-                                                    +
-                                                </Button>
-                                            </ButtonGroup>
-                                        {/if}
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        <MenuItem
+                            name={item.name}
+                            image_url={item.image_url}
+                            price={item.price}
+                            amount={cart[item.id] || 0}
+                            modifyAmount={(addAmount) =>
+                                modifyCart(item.id, addAmount)}
+                            sideStripe
+                        />
                     {/each}
                 </div>
             {/each}
---
+++ resources/js/Shared/MenuItem.svelte
@@ -0,0 +1,66 @@
+<script>
+    import { Button, ButtonGroup } from "flowbite-svelte";
+    export let amount, modifyAmount, price, image_url, name;
+    export let sideStripe = false;
+</script>
+
+<!-- The div is necessary here, so that the code outside the component
+can set it's own border (for example as a menu item separator), and not
+influence the side-stripe, which is also implemented as a border.
+-->
+<div>
+    <!-- Ideally, the classes should not be repeated,
+we should be able to do something like:
+baseClasses + (ordered) ? red border : white border
+However, for some reason, if you do that,
+the border-primary-600 style will not be present in
+the compiled css. I guess tailwind only generates them
+if it sees a usage, and the logic becomes too complicated
+for it to figure it out. -->
+    <div
+        class={sideStripe && amount > 0
+            ? "border-primary-600 border-l-4 p-1 flex md:rounded-md gap-3 bg-white"
+            : "border-white border-l-4 p-1 flex md:rounded-md gap-3 bg-white"}
+    >
+        <img class="h-20 w-20" src={image_url} alt={name} />
+        <h2 class="text-lg text-black">
+            {name}
+        </h2>
+        <div class="mr-1 ml-auto flex-col flex items-end">
+            <h2 class="font-bold text-l">
+                {price.toFixed(2)}
+            </h2>
+            <div class="mb-2 mt-auto">
+                {#if amount == 0}
+                    <Button
+                        on:click={() => modifyAmount(1)}
+                        class="rounded-full w-8 h-8 p-2 inline-flex text-center text-xl"
+                    >
+                        +
+                    </Button>
+                {:else}
+                    <ButtonGroup size="xs" class="h-8">
+                        <Button
+                            pill
+                            on:click={() => {
+                                modifyAmount(-1);
+                            }}>-</Button
+                        >
+                        <!-- Not really a button, but I just wanted to use the ButtonGroup class -->
+                        <Button pill>
+                            {amount}
+                        </Button>
+                        <Button
+                            pill
+                            on:click={() => {
+                                modifyAmount(1);
+                            }}
+                        >
+                            +
+                        </Button>
+                    </ButtonGroup>
+                {/if}
+            </div>
+        </div>
+    </div>
+</div>
